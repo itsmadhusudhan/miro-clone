f6b6c7125c466cfd2899f0b39cc8646d3edd2af5
Madhusudhan <Madhusudhan1863@gmail.com>
2024-02-02 14:03:35 +0530

    feat: add authentication with clerk

---
+++ src/providers/ConvexClientProvider.tsx
@@ -0,0 +1,28 @@
+import Loading from "@/components/auth/Loading";
+import { ClerkProvider, useAuth } from "@clerk/nextjs";
+import { AuthLoading, Authenticated, ConvexReactClient } from "convex/react";
+import { ConvexProviderWithClerk } from "convex/react-clerk";
+import React from "react";
+
+interface Props {
+  children: React.ReactNode;
+}
+
+const convexUrl = process.env.NEXT_PUBLIC_CONVEX_URL!;
+
+const convex = new ConvexReactClient(convexUrl);
+
+const ConvexClientProvider = ({ children }: Props) => {
+  return (
+    <ClerkProvider>
+      <ConvexProviderWithClerk useAuth={useAuth} client={convex}>
+        <AuthLoading>
+          <Loading />
+        </AuthLoading>
+        <Authenticated>{children}</Authenticated>
+      </ConvexProviderWithClerk>
+    </ClerkProvider>
+  );
+};
+
+export default ConvexClientProvider;
